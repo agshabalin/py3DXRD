49f9a73fb547a6561a6dd69ab040d9f7a1d73e4d
Anatoly Shabalin <shabalin@max-p3a006.desy.de>
2022-08-03 14:48:44 +0200

    first commit

--- setup.py
+++ setup.py
@@ -4,18 +4,16 @@ from distutils.core import setup
 import numpy as np
 
 setup(name='Geometry Converter',
-      version=get_version(),
+      version='0.0',
       author='Anatoly Shabalin',
       author_email='a.shabalin.r@egmail.com',
-      cmdclass={ 'build_ext' : build_ext_subclass },
       description='Converts geometry between HEXRD, ImageD11, PolyXSim, and pyFAI.',
       long_description = open('README.md').read(),
       license = "GPL",
-      ext_package = "ImageD11",
       setup_requires = ["numpy", "scipy", "setuptools"],   # to compile
       install_requires = ["numpy", "scipy", "setuptools"],
       extras_require = { 'full' : [], 'rare' : [] },
-      packages = [],
+      packages = ['distutils', 'distutils.command'],
       package_dir = {"geometry_converter":"geometry_converter"},
       url = "https://github.com/agshabalin/py3DXRD.git",
       scripts = ["py3DXRD/convert_geometry.py"],
